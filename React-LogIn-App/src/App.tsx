cda7650a45b2924b2041eaf8fb01781dfd682825
Crystal95129 <crystal@yahoo.com>
2024-07-26 09:46:53 -0700

    adding react files

---
+++ React-LogIn-App/src/App.tsx
@@ -0,0 +1,13 @@
+import Nav from './component/Nav';
+import Login from './component/Login';
+import './App.css'
+
+function App() {
+  return <div className="App">
+       <Nav />
+       <Login />
+  </div>
+}
+
+
+export default App;
---
+++ React-LogIn-App/src/component/Login.tsx
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import "./Login.css";
+import { redirect } from "react-router-dom";
+
+class Login extends Component {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      islogged: false,
+
+      loginParams: {
+        user_id: "",
+
+        user_password: "",
+      },
+    };
+  }
+
+  handleFormChange = (event: { target: { value: any; name: string | number; }; }) => {
+    let loginParamsNew = { ...this.state.loginParams };
+
+    let val = event.target.value;
+
+    loginParamsNew[event.target.name] = val;
+
+    this.setState({
+      loginParams: loginParamsNew,
+    });
+  };
+
+  login = (event: { preventDefault: () => void; }) => {
+    let user_id = this.state.loginParams.user_id;
+
+    let user_password = this.state.loginParams.user_password;
+
+    if (user_id === "admin" && user_password === "123") {
+      localStorage.setItem("token", "T");
+
+      this.setState({
+        islogged: true,
+      });
+    }
+
+    event.preventDefault();
+  };
+
+  render() {
+    if (localStorage.getItem("token")) {
+      return <redirect to="/" />;
+    }
+
+    return (
+      <div className="container">
+        <form onSubmit={this.login} className="form-signin">
+          <h1 className="h3 mb-3 font-weight-normal">Please Login</h1>
+
+          <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                name="user_id"
+                onChange={this.handleFormChange}
+                placeholder="Enter Username"
+              />
+
+              <input
+                type="password"
+                name="user_password"
+                onChange={this.handleFormChange}
+                placeholder="Enter Password"
+              />
+
+              <input type="submit" value="Login" />
+            </div>
+          </div>
+
+          <p>user_id === "admin" && user_password === "123"</p>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
